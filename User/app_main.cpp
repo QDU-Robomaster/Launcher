bd8b66ee54106cd2557874de508d8eecbadece88
llLeo <li13780602203@qq.com>
2025-10-05 00:30:20 +0800

    update

--- User/app_main.cpp
+++ User/app_main.cpp
@@ -61,16 +61,16 @@ static uint8_t usart6_rx_buf[512];
 static uint8_t i2c1_buf[32];
 static uint8_t i2c2_buf[32];
 static uint8_t i2c3_buf[32];
-static uint8_t usb_otg_hs_ep0_in_buf[8];
-static uint8_t usb_otg_hs_ep0_out_buf[8];
-static uint8_t usb_otg_hs_ep1_in_buf[128];
-static uint8_t usb_otg_hs_ep1_out_buf[128];
-static uint8_t usb_otg_hs_ep2_in_buf[16];
 static uint8_t usb_otg_fs_ep0_in_buf[8];
 static uint8_t usb_otg_fs_ep0_out_buf[8];
 static uint8_t usb_otg_fs_ep1_in_buf[128];
 static uint8_t usb_otg_fs_ep1_out_buf[128];
 static uint8_t usb_otg_fs_ep2_in_buf[16];
+static uint8_t usb_otg_hs_ep0_in_buf[8];
+static uint8_t usb_otg_hs_ep0_out_buf[8];
+static uint8_t usb_otg_hs_ep1_in_buf[128];
+static uint8_t usb_otg_hs_ep1_out_buf[128];
+static uint8_t usb_otg_hs_ep2_in_buf[16];
 
 extern "C" void app_main(void) {
   // clang-format on
@@ -139,22 +139,6 @@ extern "C" void app_main(void) {
 
   STM32CAN can2(&hcan2, 5);
 
-  static constexpr auto USB_OTG_HS_LANG_PACK = LibXR::USB::DescriptorStrings::MakeLanguagePack(LibXR::USB::DescriptorStrings::Language::EN_US, "XRobot", "STM32 XRUSB USB_OTG_HS CDC Demo", "123456789");
-  LibXR::USB::CDCUart usb_otg_hs_cdc(128, 128, 3);
-
-  STM32USBDeviceOtgHS usb_hs(
-      &hpcd_USB_OTG_HS,
-      256,
-      {usb_otg_hs_ep0_out_buf, usb_otg_hs_ep1_out_buf},
-      {{usb_otg_hs_ep0_in_buf, 8}, {usb_otg_hs_ep1_in_buf, 128}, {usb_otg_hs_ep2_in_buf, 16}},
-      USB::DeviceDescriptor::PacketSize0::SIZE_8,
-      0x483, 0x5740, 0xF407,
-      {&USB_OTG_HS_LANG_PACK},
-      {{&usb_otg_hs_cdc}}
-  );
-  usb_hs.Init();
-  usb_hs.Start();
-
   static constexpr auto USB_OTG_FS_LANG_PACK = LibXR::USB::DescriptorStrings::MakeLanguagePack(LibXR::USB::DescriptorStrings::Language::EN_US, "XRobot", "STM32 XRUSB USB_OTG_FS CDC Demo", "123456789");
   LibXR::USB::CDCUart usb_otg_fs_cdc(128, 128, 3);
 
@@ -170,6 +154,22 @@ extern "C" void app_main(void) {
   );
   usb_fs.Init();
   usb_fs.Start();
+
+  static constexpr auto USB_OTG_HS_LANG_PACK = LibXR::USB::DescriptorStrings::MakeLanguagePack(LibXR::USB::DescriptorStrings::Language::EN_US, "XRobot", "STM32 XRUSB USB_OTG_HS CDC Demo", "123456789");
+  LibXR::USB::CDCUart usb_otg_hs_cdc(128, 128, 3);
+
+  STM32USBDeviceOtgHS usb_hs(
+      &hpcd_USB_OTG_HS,
+      256,
+      {usb_otg_hs_ep0_out_buf, usb_otg_hs_ep1_out_buf},
+      {{usb_otg_hs_ep0_in_buf, 8}, {usb_otg_hs_ep1_in_buf, 128}, {usb_otg_hs_ep2_in_buf, 16}},
+      USB::DeviceDescriptor::PacketSize0::SIZE_8,
+      0x483, 0x5740, 0xF407,
+      {&USB_OTG_HS_LANG_PACK},
+      {{&usb_otg_hs_cdc}}
+  );
+  usb_hs.Init();
+  usb_hs.Start();
 
   /* Terminal Configuration */
   STDIO::read_ = usb_otg_fs_cdc.read_port_;
